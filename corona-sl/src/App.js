5ed1ed679eb14c7cea4121ff78b291186c0935ca
Janitha Tennakoon <janitha000@gmail.com>
2020-03-18 15:56:56 +0800

    Added basic project structure

---
+++ corona-sl/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Home from './Home/Home'
+
+function App() {
+  return (
+    <div className="App">
+     Corona SL 
+     <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ corona-sl/src/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import StatCard from '../StatCard/StatCard'
+
+const Home = () => {
+    return(
+        <StatCard />
+    )
+}
+
+export default Home;
